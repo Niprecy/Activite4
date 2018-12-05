ba4639270a0ffda8df725074619406836224d8d6
Cyprien Bleuze <cyprienbleuze28@gmail.com>
2018-12-05 13:27:07 +0100

    première sauvegarde

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+	<head>
+		<title>Mon activité 4</title>
+		<link rel="stylesheet" href="style.css" />
+		<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<meta name="generator" content="Geany 1.29" />
+	</head>
+
+		<body>
+			 
+			<header>
+				
+			<ul id="menu">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Services</a></li>
+				<li><a href="#">Clients</a></li>
+				<li><a href="#">Team</a></li>
+				<li><a href="#">Contact</a></li>		
+			</ul>
+<!--
+			 <img alt="img_de_fond_en_haut" src="images/kazuend-40019-unsplash.jpg" class="image"/>
+-->
+
+			<br></br>
+			<br></br>
+			<p>Can you built the website of my dreams ?</p>
+				
+			<h1> YUP, WE CAN DO THAT. </h1>
+			
+			</header>
+
+test
+	
+		</body>
+
+</html>
